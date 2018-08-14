437da32af9330932ba4d15fa27c22f427cf7d874
OLAJUWON <BADGUY@jays-macbook-pro.local>
2018-08-14 13:48:13 +0100

    Restructure to MVC

---
+++ FlashChat/Model/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by OLAJUWON on 8/14/18.
+//  Copyright © 2018 OLAJUWON. All rights reserved.
+//
+
+
+import Foundation
+
+//  This is the model class that represents the blueprint for a message
+
+class Message {
+    
+    //TODO: Messages need a messageBody and a sender variable
+    
+    var sender: String = ""
+    var messageBody: String = ""
+    
+}
